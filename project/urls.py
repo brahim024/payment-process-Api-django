7d15aa3e1edc30c62faba1d75200d8c416470960
brahim024 <ifninos168@gmail.com>
2020-11-09 15:52:30 +0100

    replace folders from my ecommerce project

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 """project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path ,include
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('order/' ,include('order.urls',namespace='orders')),
+    path('cart/',include('cart.urls',namespace='cart')),
+    path('stor/',include('ecomapp.urls',namespace='ecomapp')),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
